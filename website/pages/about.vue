f17ac1ed14f21a2a7e9817a7beb1e2c023ec8d73
EyesOpenCure <37727916+EyesOpenCure@users.noreply.github.com>
2018-04-09 20:32:30 -0700

    Update about.vue

--- website/pages/about.vue
+++ website/pages/about.vue
@@ -3,11 +3,11 @@
     <div class="section">
       <div class="container">
         <p>
-          Aenean eu euismod felis. Sed tincidunt neque vitae mi fringilla finibus. In convallis posuere libero, commodo placerat nulla dictum quis. Morbi eleifend quis enim sit amet commodo. Phasellus libero nulla, sodales a magna a, congue commodo felis. Quisque id gravida ipsum. Proin fringilla, lectus id feugiat varius, augue odio interdum orci, sed convallis erat quam quis sapien. Nam porttitor iaculis gravida. Aliquam et arcu viverra, condimentum arcu ac, accumsan nunc. Maecenas id felis ut tellus tincidunt interdum ut vel massa. Etiam malesuada faucibus sapien nec interdum. Mauris quis velit diam. Nullam varius vestibulum elit a auctor. Phasellus maximus ante nec commodo fermentum. Nunc consequat aliquam varius.
-        </p>
+	  We are a group of internet volunteers from a diverse range of academic backgrounds, brought together by the common goal of treating blindness. We are not doing this because we expect that this will be an easy problem to solve, but rather because we think it is a worthwhile problem that deserves a fresh approach. We have new ideas, we are resourceful, and most of all we are very motivated.
+	</p>
         <p>
-          Sed porta mattis dignissim. Vivamus bibendum nisl orci, et aliquet neque rhoncus a. Pellentesque aliquam rhoncus imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam pretium pulvinar arcu, iaculis scelerisque mauris hendrerit sit amet. Ut mollis tortor sed tortor volutpat, non tempor nunc blandit. Nullam sapien diam, malesuada tempor tincidunt auctor, scelerisque sed odio. Fusce at molestie odio. Nullam volutpat, ipsum a malesuada blandit, nulla lectus hendrerit libero, ut rutrum metus nisi at felis. In blandit turpis sit amet ex lobortis dapibus. Mauris lacinia pellentesque mauris in eleifend. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent condimentum et quam at semper. 
-        </p>
+	  Whether you have a background in biology, chemistry, physics, engineering, or even are just casually interested in the project, we would love your help. Let's do this together.
+	</p>
       </div>
     </div>
   </div>
